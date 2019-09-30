a28e1804231fd94f4e03cf80d9ef8794b0e82ab7
hanneslantz <hannes.lantz.97@gmail.com>
2019-09-30 17:30:29 +0200

    stated on lab4

--- src/CodeBreaker.java
+++ src/CodeBreaker.java
@@ -52,5 +52,47 @@ public class CodeBreaker implements SnifferCallback {
     @Override
     public void onMessageIntercepted(String message, BigInteger n) {
         System.out.println("message intercepted (N=" + n + ")...");
+        WorklistItem wli = new WorklistItem(n, "N: " + n);
+        Jbutton button = new Jbutton("GET CRACKING");
+        ProgressTracker tracker = new tracker();
+        button.addActionListener(e -> {
+        	moveItem(wli, button);
+        	startCracking(message, n, tracker);
+        });
+        wli.add(button);
+        wli.(wli);
+    }
+    
+    private void startCracking(String code, BigInterger n, ProgressTracker tracker) {
+    	
+    }
+    
+    private void moveItem(WorklistItem wli, JButton button) {
+    	
+    }
+    
+    // -----------------------------------------------------------------------
+
+    /** ProgressTracker: reports how far factorization has progressed */ 
+    private static class Tracker implements ProgressTracker {
+        private int totalProgress = 0;
+        private int prevPercent = -1;
+
+        /**
+         * Called by Factorizer to indicate progress. The total sum of
+         * ppmDelta from all calls will add upp to 1000000 (one million).
+         * 
+         * @param  ppmDelta   portion of work done since last call,
+         *                    measured in ppm (parts per million)
+         */
+        @Override
+        public void onProgress(int ppmDelta) {
+            totalProgress += ppmDelta;
+            int percent = totalProgress / 10000;
+            if (percent != prevPercent) {
+                System.out.println(percent + "%");
+                prevPercent = percent;
+            }
+        }
     }
 }
